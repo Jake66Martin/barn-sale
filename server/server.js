491e8eb0457ff4ae90c04c404c9cae8ea3d5893c
leviathan <jake66martin@hotmail.com>
2024-02-17 12:37:10 -0500

    added user model

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {expressMiddleware} = require('@apollo/server/express4');
 const path = require('path');
-const {apolloServer} = require('@apollo/server');
+const {ApolloServer} = require('@apollo/server');
 const {authMiddleware} = require('./utils/auth');
 
 const {typeDefs, resolvers} = require('./schemas');
@@ -9,7 +9,7 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new apolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
