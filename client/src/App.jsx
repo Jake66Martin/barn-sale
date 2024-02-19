2f2cb99b9e907a27ddb4c663d5b7e293b42f1b15
leviathan <jake66martin@hotmail.com>
2024-02-19 15:15:25 -0500

    adding base components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { Outlet } from "react-router-dom";
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
 import "./App.css";
 
 const httpLink = createHttpLink({
