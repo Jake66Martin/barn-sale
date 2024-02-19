76ab7c430c67a254e7df1d0b5147c1c22b67e781
leviathan <jake66martin@hotmail.com>
2024-02-19 15:09:42 -0500

    working on main.jsx and app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+const httpLink = createHttpLink({
+  uri: "graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const oken = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? "Bearer ${token}" : "",
+    },
+  };
+});
+
+const client = new AppoloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1></h1>
-  )
+    <ApolloProvider client={client}>
+      <>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
