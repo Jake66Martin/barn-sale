46a6dd873e2d19ada9551c691840b4da92ed3b07
leviathan <jake66martin@hotmail.com>
2024-03-11 15:27:14 -0400

    adjusting browsing section

--- client/src/pages/browse/browse.jsx
+++ client/src/pages/browse/browse.jsx
@@ -1,7 +1,135 @@
 import './browse.css'
 
+import { useEffect, useState } from "react";
+
+
 export default function Browse() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+          setIsMobile(window.innerWidth <= 650);
+        };
+    
+        window.addEventListener("resize", handleResize);
+        handleResize();
+    
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
+    
+
     return(
-        <h1>Browse</h1>
+        <>
+      {!isMobile ? (
+        <div className="d-flex justify-content-center align-items-center text-white position-absolute w-100 height">
+          <div className="w-100 h-100 d-flex flex-column">
+            <div className="w-100 h-50 d-flex">
+              <div className="application-div d-flex justify-content-center align-items-center position-relative div-width">
+                <img
+                  src="/basement.jpg"
+                  alt="basement category"
+                  className="img-size rounded"
+                />
+              </div>
+              <div className="application-div d-flex justify-content-center align-items-center position-relative div-width">
+                <img
+                  src="/bathroom.png"
+                  alt="bathroom category"
+                  className="img-size rounded"
+                />
+                
+              </div>
+              <div className="application-div d-flex justify-content-center align-items-center position-relative div-width">
+                <img
+                  src="/bedroom.jpg"
+                  alt="bedroom category"
+                  className="img-size rounded"
+                />
+                
+              </div>
+            </div>
+            <div className="w-100 h-50 d-flex">
+              <div className="application-div d-flex justify-content-center align-items-center position-relative div-width">
+                <img
+                  src="/dining-room.jpg"
+                  alt="dining room category"
+                  className="img-size rounded"
+                />
+                
+              </div>
+              <div className="application-div d-flex justify-content-center align-items-center position-relative div-width">
+                <img
+                  src="/kitchen.jpg"
+                  alt="kitchen category"
+                  className="img-size rounded"
+                />
+                
+              </div>
+              <div className="application-div d-flex justify-content-center align-items-center position-relative div-width">
+                <img
+                  src="/office.jpg"
+                  alt="office category"
+                  className="img-size rounded"
+                />
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="d-flex flex-column overflow-container align-items-center text-white position-absolute w-100 height">
+          <div className="application-div m-3 d-flex justify-content-center align-items-center position-relative">
+            <img
+              src="/basement.jpg"
+              alt="basement category"
+              className="img-size rounded"
+            />
+           
+          </div>
+          <div className="m-3 application-div d-flex justify-content-center align-items-center position-relative">
+            <img
+              src="/bathroom.png"
+              alt="bathroom category"
+              className="img-size rounded"
+            />
+            
+          </div>
+          <div className="m-3 application-div d-flex justify-content-center align-items-center position-relative">
+            <img
+              src="/bedroom.jpg"
+              alt="bedroom category"
+              className="img-size rounded"
+            />
+           
+          </div>
+          <div className="m-3 application-div d-flex justify-content-center align-items-center position-relative">
+            <img
+              src="/dining-room.jpg"
+              alt="dining room category"
+              className="img-size rounded"
+            />
+            
+          </div>
+          <div className="m-3 application-div d-flex justify-content-center align-items-center position-relative">
+            <img
+              src="/kitchen.jpg"
+              alt="kitchen category"
+              className="img-size rounded"
+            />
+            
+          </div>
+          <div className="m-3 application-div d-flex justify-content-center align-items-center position-relative">
+            <img
+              src="/office.jpg"
+              alt="office category"
+              className="img-size rounded"
+            />
+            
+          </div>
+        </div>
+      )}
+    </>
     )
 }
