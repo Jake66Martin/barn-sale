a00b6f7b3ba320cae7c2d6c8f16b3a02b355fd65
leviathan <jake66martin@hotmail.com>
2024-03-13 14:51:49 -0400

    changing mongo to sql

---
+++ server/db/schema.sql
@@ -0,0 +1,5 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS thriftbarnfurniture_db;
+
+-- CREATE DATABASE
+CREATE DATABASE thriftbarnfurniture_db;
--- server/models/Item.js
+++ server/models/Item.js
@@ -1,28 +1,73 @@
-const { Schema, model } = require("mongoose");
+// const { Schema, model } = require("mongoose");
 
-const itemSchema = new Schema({
-  item: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-});
+// const itemSchema = new Schema({
+//   item: {
+//     type: String,
+//     required: true
+//   },
+//   category: {
+//     type: String,
+//     required: true,
+//   },
+//   description: {
+//     type: String,
+//     required: true,
+//   },
+//   price: {
+//     type: Number,
+//     required: true,
+//   },
+//   image: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-const Item = model("Item", itemSchema);
+// const Item = model("Item", itemSchema);
 
-module.exports = Item;
+// module.exports = Item;
+
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Item extends Model {}
+
+Item.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    item: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    image: {
+
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
+
+module.exports = Category;
