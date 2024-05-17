ea9c1e73a3c6a6e23b48056fffa1b3e84f904588
leviathan <jake66martin@hotmail.com>
2024-05-17 10:04:13 -0400

    made changes

--- client/src/pages/homepage/homepage.jsx
+++ client/src/pages/homepage/homepage.jsx
@@ -1,8 +1,16 @@
-import './homepage.css'
+import "./homepage.css";
 
 export default function Homepage() {
-    return(
-        <div className='background bg-height'>
+  
+
+  return (
+    <div className="background bg-height">
+        <div className="d-flex justify-content-center align-items-center welcome">
+            <h1 className='headings'> Welcome to thrift barn furniture!</h1>
         </div>
-    )
-};
+        <div className='d-flex justify-content-center treasure'>
+            <h1 className='headings'>Where you can discover treasure in every corner.</h1>
+        </div>
+    </div>
+  );
+}
--- server/config/connection.js
+++ server/config/connection.js
@@ -5,10 +5,16 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
+      },
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
       },
     });
 
