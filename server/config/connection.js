cb5abce6d04ea146978f15e2989cc5a2a3424a1a
leviathan <jake66martin@hotmail.com>
2024-05-17 11:32:15 -0400

    changing port

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,16 +6,10 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT || 3306, 
+      port: process.env.DB_PORT || 10000, 
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
-      },
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
       },
     });
 
