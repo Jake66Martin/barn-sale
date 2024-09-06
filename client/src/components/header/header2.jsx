2921293b547e14172de5b08de441f92d1356a780
leviathan <jake66martin@hotmail.com>
2024-09-06 15:35:55 -0400

    working on homepage

--- client/src/components/header/header2.jsx
+++ client/src/components/header/header2.jsx
@@ -45,13 +45,22 @@ export default function Header2() {
         //   </li>
         // </ul>
         <>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} to="/Browse">
+          <Link style={{ color: "#da0404", textDecoration: "none" }} 
+          to="/Browse"
+          className={styles.textstyle}
+          >
             Browse our furniture
           </Link>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} to="/About">
+          <Link style={{ color: "#da0404", textDecoration: "none" }} 
+          to="/About"
+          className={styles.textstyle}
+          >
             About us
           </Link>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} to="/Contact">
+          <Link style={{ color: "#da0404", textDecoration: "none" }} 
+          to="/Contact"
+          className={styles.textstyle}
+          >
             Contact
           </Link>
         </>
@@ -59,13 +68,23 @@ export default function Header2() {
     } else {
       return (
         <>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} to="/Browse">
+          <Link style={{ color: "#da0404", textDecoration: "none" }} 
+          to="/Browse"
+          className={styles.textstyle}
+          >
             Browse our furniture
           </Link>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} to="/About">
+          <Link style={{ color: "#da0404", textDecoration: "none" }} 
+          to="/About"
+          className={styles.textstyle}
+          >
             About us
           </Link>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} to="/Contact">
+          <Link style={{ color: "#da0404", textDecoration: "none" }} 
+          to="/Contact"
+          className={styles.textstyle}
+          >
+      
             Contact
           </Link>
         </>
@@ -103,14 +122,16 @@ export default function Header2() {
   return (
     <div className={`${styles.height} ${styles.grid}`}>
       <div className={styles.toprow}>
-        <h3 style={{ color: "white", textAlign: "center" }}>
+        <h3 style={{ color: "white", textAlign: "center" }}
+        className={styles.textstyle}
+        >
           Announcements can go up here
         </h3>
       </div>
       <div className={`${styles.bottomrow} ${styles.grid2}`}>
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
           <img
-            src="/scottlogo.jpg"
+            src="/headerlogo.png"
             alt="thrift barn logo"
             className={styles.logosize}
           />
--- client/src/pages/homepage/homepage2.jsx
+++ client/src/pages/homepage/homepage2.jsx
@@ -1,24 +1,57 @@
+import { Link } from "react-router-dom";
+
 import styles from "./homepage2.module.css";
 
 export default function Homepage2() {
   return (
     <>
-      <div className={styles.background}>
+      <hero className={styles.background}>
         <img
-          src="/scottlogo.jpg"
+          src="/crestlogo.png"
           alt="thrift barn logo"
           className={`${styles.logosize} ${styles.item1}`}
         />
-        <h1
-          style={{ color: "white", textAlign: "center" }}
-          className={styles.item2}
+        <div className={styles.item2}>
+          <h1
+            style={{ color: "white", textAlign: "center" }}
+            className={`${styles.textstyle} ${styles.textsize}`}
+          >
+            Furniture Problems?
+          </h1>
+          <p
+            className={`${styles.textstyle2} ${styles.textsize2}`}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            We have the solution.
+          </p>
+        </div>
+
+        <Link
+          style={{
+            display: "grid",
+            justifyItems: "end",
+            textDecoration: "none",
+            color: "white",
+          }}
+          className={`${styles.item3} ${styles.textstyle2} ${styles.border}`}
+          to="/Browse"
         >
-          Furniture Problems? <br /> We have the solution.
-        </h1>
-        <button className={styles.item3}>Browse Online</button>
-        <button className={styles.item4}>Contact Us!</button>
-      </div>
-      <div className={styles.info}>
+          Browse Online
+        </Link>
+        <Link
+          style={{
+            display: "grid",
+            justifyItems: "start",
+            textDecoration: "none",
+            color: "white",
+          }}
+          className={`${styles.item4} ${styles.textstyle2} ${styles.border}`}
+          to="/Contact"
+        >
+          Contact Us!
+        </Link>
+      </hero>
+      <section className={styles.info}>
         <div
           style={{
             display: "grid",
@@ -27,50 +60,146 @@ export default function Homepage2() {
           }}
           className={styles.item5}
         >
-          <h2 style={{ textAlign: "center" }}>
-            Discover treasures in every corner.
-          </h2>
-          <p style={{ textAlign: "center" }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
-            mollis nisl <br />
-            in mollis. Vivamus malesuada augue ut tincidunt dapibus. Donec
-            posuere elementum <br />
-            velit, eu dignissim dui viverra non. Sed ac sagittis ex. Duis
-            dignissim ullamcorper
-            <br />
-            nibh, quis rhoncus neque lacinia id. Aliquam laoreet fermentum
-            libero.
-          </p>
+          <div style={{ display: "grid", justifyItems: "center" }}>
+            <img src="/leaf.png" className={styles.leaflogo} />
+
+            <h2
+              style={{
+                textAlign: "center",
+                display: "grid",
+                alignItems: "end",
+                color: "#720909",
+              }}
+              className={styles.textstyle}
+            >
+              Discover treasures in every corner.
+            </h2>
+            <p style={{ textAlign: "center" }} className={styles.textstyle2}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              dapibus mollis nisl <br />
+              in mollis. Vivamus malesuada augue ut tincidunt dapibus. Donec
+              posuere elementum <br />
+              velit, eu dignissim dui viverra non. Sed ac sagittis ex. Duis
+              dignissim ullamcorper
+              <br />
+              nibh, quis rhoncus neque lacinia id. Aliquam laoreet fermentum
+              libero.
+            </p>
+          </div>
         </div>
-        <div style={{
+        <div
+          style={{
             display: "grid",
             justifyContent: "center",
-            alignItems: 'center'
-          }}>
-          <h3>Living Room</h3>
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/livingroom2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0' }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Living Room
+            </h3>
+          </div>
         </div>
-        <div style={{
+        <div
+          style={{
             display: "grid",
             justifyContent: "center",
-            alignItems: 'center'
-          }}>
-          <h3>Dining Room</h3>
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/dining2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0' }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Dining Room
+            </h3>
+          </div>
         </div>
-        <div style={{
+        <div
+          style={{
             display: "grid",
             justifyContent: "center",
-            alignItems: 'center'
-          }}>
-          <h3>Kitchen & Bath</h3>
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/kitchen2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0'  }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Kitchen & Bath
+            </h3>
+          </div>
         </div>
-        <div style={{
+        <div
+          style={{
             display: "grid",
             justifyContent: "center",
-            alignItems: 'center'
-          }}>
-          <h3>Bedroom</h3>
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/bedroom2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0'  }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Bedroom
+            </h3>
+          </div>
         </div>
-      </div>
+        <div
+          style={{
+            display: "grid",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/nursery2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0'  }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Child/Nursery
+            </h3>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/office2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0'  }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Office
+            </h3>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/garage2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0'  }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Garage/Exterior
+            </h3>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <img src="/decor2.png" className={styles.imgsize} />
+            <h3 style={{ textAlign: "center", color: '#720909', margin:'20px 0'  }} className={`${styles.textstyle2} ${styles.labelweight}`}>
+              Home Decor
+            </h3>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
