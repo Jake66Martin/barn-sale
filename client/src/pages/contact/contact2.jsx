86a94357f29908d859c167d34134862a1708ebd0
leviathan <jake66martin@hotmail.com>
2024-09-26 11:42:22 -0400

    finished contact page

--- client/src/pages/contact/contact2.jsx
+++ client/src/pages/contact/contact2.jsx
@@ -10,40 +10,36 @@ export default function Contact2() {
       </div>
       <div className={`${styles.mheight}`}>
         <div className={`${styles.cell1}`}>
-          <div>
-            <img src="phoneicon.png" alt="phone icon" />
-            <p>Text or call 24/7 -365:</p>
-            <p>613-666-6666</p>
-          </div>
+            <img src="phoneicon.png" alt="phone icon" style={{alignSelf: 'center', justifySelf: 'center'}}/>
+            <p style={{textAlign: 'center', fontWeight: '400', lineHeight: '2'}} className={`${styles.textstyle2}`}><span style={{color: '#da0404', textAlign: 'center', fontWeight: '600'}} className={`${styles.textstyle2}`}>Text or call 24/7 -365:</span><br/>
+            613-666-6666</p>
         </div>
         <div className={`${styles.cell2}`}>
-          <div>
-            <img src="barnicon.png" alt="barn icon" />
-            <p>Address:</p>
-            <p>123 Street Name <br/>
+            <img src="barnicon.png" alt="barn icon" style={{alignSelf: 'center', justifySelf: 'center'}}/>
+            <p style={{textAlign: 'center', fontWeight: '400', lineHeight: '2'}} className={`${styles.textstyle2}`}><span  style={{color: '#da0404', fontWeight: '600'}}>Address:</span> <br/>
+             123 Street Name <br/>
             City Name, Province <br/>
             123 ABC
             </p>
-          </div>
         </div>
         <div className={`${styles.cell3}`}>
-          <div>
-            <img src="hoursicon.png" alt="clock icon" />
-            <p>In-Store Hours:</p>
-            <p>Weekdays: AM - Pm <br/>
+            <img src="hoursicon.png" alt="clock icon" style={{alignSelf: 'center', justifySelf: 'center'}}/>
+            <p style={{textAlign: 'center', fontWeight: '400', lineHeight: '2'}} className={`${styles.textstyle2}`}><span style={{color: '#da0404', textAlign: 'center', fontWeight: '600'}} className={`${styles.textstyle2}`}>In-Store Hours:</span><br/>
+            Weekdays: AM - Pm <br/>
             Weekends: AM - PM</p>
-          </div>
         </div>
         <div className={`${styles.cell4}`}>
-          <div>
-            <img src="mailicon.png" alt="email icon" />
-            <p>Email:</p>
-            <p>Thriftbarnfurniture@gmail.com</p>
-          </div>
+            <img src="mailicon.png" alt="email icon" style={{alignSelf: 'center', justifySelf: 'center'}}/>
+            <p style={{textAlign: 'center', fontWeight: '400', lineHeight: '2'}} className={`${styles.textstyle2}`}><span style={{color: '#da0404', textAlign: 'center', fontWeight: '600'}} className={`${styles.textstyle2}`}>Email:</span><br/>
+            Thriftbarnfurniture@gmail.com</p>
         </div>
         <div className={`${styles.cell5}`}>
-            <h2>Lorum Ipsum</h2>
-            <p style={{ textAlign: "center" }} className={styles.textstyle2}>
+            <div style={{alignSelf: 'center', justifySelf: 'center'}}>
+            <h2 className={`${styles.textstyle}`} style={{color: '#720909',
+                fontSize: '35px',
+                margin: '15px 0'
+            }}>Lorum Ipsum</h2>
+            <p className={styles.textstyle2}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
               dapibus mollis nisl <br />
               in mollis. Vivamus malesuada augue ut tincidunt dapibus. Donec
@@ -53,18 +49,25 @@ export default function Contact2() {
               <br />
               nibh, quis rhoncus neque lacinia id. Aliquam laoreet fermentum
               libero.
+              <br />
+              velit, eu dignissim dui viverra non. Sed ac sagittis ex. Duis
+              dignissim ullamcorper
+              <br />
+              nibh, quis rhoncus neque lacinia id. Aliquam laoreet fermentum
+              libero.
             </p>
-            <img src='decor.jpg' alt='image' className={`${styles.decimg}`}/>
+            <img src='decor.jpg' alt='image' className={`${styles.decimg}`} style={{margin: '15px 0'}}/>
+            </div>
         </div>
         <div className={`${styles.cell6}`}>
-            <form>
-                <label>Full Name</label>
-                <input type='text'></input>
-                <label>Email</label>
-                <input type='text'></input>
-                <label>Message</label>
-                <input type='text'></input>
-                <button type='submit'>Submit Message</button>
+            <form style={{lineHeight: '2', alignSelf: 'center', justifySelf: 'center'}}>
+                <label style={{display: 'block'}}>Full Name</label>
+                <input type='text' style={{display: 'block', width: '700px'}}></input>
+                <label style={{display: 'block'}}>Email</label>
+                <input type='text' style={{display: 'block', width: '700px'}}></input>
+                <label style={{display: 'block'}}>Message</label>
+                <textarea type='text' style={{display: 'block', width: '700px', height: '300px', resize: 'none'}}></textarea>
+                <button type='submit' style={{display: 'block', margin: '12px 0'}}>Submit Message</button>
             </form>
         </div>
         <div className={`${styles.cell7}`}></div>
