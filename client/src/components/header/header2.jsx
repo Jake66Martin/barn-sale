ae2e8b5a50350a0cc43840e9834a25b9f44c0096
leviathan <jake66martin@hotmail.com>
2024-10-03 10:28:00 -0400

    fixed dropdown menu

--- client/src/components/header/header2.jsx
+++ client/src/components/header/header2.jsx
@@ -1,44 +1,43 @@
 import styles from "./header2.module.css";
 import Auth from "../../utils/auth";
 import { Link, useLocation } from "react-router-dom";
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Header2() {
   const cp = useLocation().pathname;
 
-  
-    // State to track whether the div is visible or not
-    const [isVisible, setIsVisible] = useState(false);
-  
-    // Toggle function to change the visibility
-    const toggleVisibility = () => {
-      setIsVisible(!isVisible); // Switch between true and false
-    }
+  // State to track whether the div is visible or not
+  const [isVisible, setIsVisible] = useState(false);
 
-
+  // Toggle function to change the visibility
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible); // Switch between true and false
+  };
 
   function showOptions() {
     if (Auth.loggedIn()) {
       return (
-        
         <>
-          <p style={{ color: "#da0404", textDecoration: "none" }} 
-         to='#'
-         onClick={toggleVisibility}
-          className={styles.textstyle}
+          <p
+            style={{ color: "#da0404", textDecoration: "none" }}
+            to="#"
+            onClick={toggleVisibility}
+            className={styles.textstyle}
           >
-            {isVisible ? 'Hide' : 'Show'}
+            {isVisible ? "Hide" : "Show"}
             Browse our furniture
           </p>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} 
-          to="/About"
-          className={styles.textstyle}
+          <Link
+            style={{ color: "#da0404", textDecoration: "none" }}
+            to="/About"
+            className={styles.textstyle}
           >
             About us
           </Link>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} 
-          to="/Contact"
-          className={styles.textstyle}
+          <Link
+            style={{ color: "#da0404", textDecoration: "none" }}
+            to="/Contact"
+            className={styles.textstyle}
           >
             Contact
           </Link>
@@ -47,77 +46,194 @@ export default function Header2() {
     } else {
       return (
         <>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} 
-         to='#'
-          onClick={toggleVisibility}
-
-          className={styles.textstyle}
+          <Link
+            style={{
+              color: "#da0404",
+              textDecoration: "none",
+              position: "relative",
+            }}
+            to="#"
+            onClick={toggleVisibility}
+            className={styles.textstyle}
           >
             Browse our furniture
+            <div
+              className={`${styles.ddmenu}`}
+              style={{ display: isVisible ? "block" : "none" }}
+            >
+              <ul
+                style={{
+                  margin: "0 0",
+                  padding: "0 0",
+                  height: "100%",
+                  width: "100%",
+                }}
+                className={`${styles.ulmen}`}
+              >
+                <Link
+                  to="/Subcategories/LivingRoom"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Living Room
+                  </li>
+                </Link>
+                <Link
+                  to="/Subcategories/DiningRoom"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Dining Room
+                  </li>
+                </Link>
+                <Link
+                  to="/Subcategories/Kitchen&Bath"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Kitchen & Bath
+                  </li>
+                </Link>
+                <Link
+                  to="/Subcategories/Bedroom"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Bedroom
+                  </li>
+                </Link>
+                <Link
+                  to="/Subcategories/Child&Nursery"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Child & Nursery
+                  </li>
+                </Link>
+                <Link
+                  to="/Subcategories/Office"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Office
+                  </li>
+                </Link>
+                <Link
+                  to="/Subcategories/Garage&Exterior"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Garage & Exterior
+                  </li>
+                </Link>
+                <Link
+                  to="/Subcategories/HomeDecor"
+                  style={{ textDecoration: "none" }}
+                >
+                  <li
+                    style={{ listStyleType: "none", color: "#da0404" }}
+                    className={`${styles.textstyle}`}
+                  >
+                    Home Decor
+                  </li>
+                </Link>
+              </ul>
+            </div>
           </Link>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} 
-          to="/About"
-          className={styles.textstyle}
+          <Link
+            style={{ color: "#da0404", textDecoration: "none" }}
+            to="/About"
+            className={styles.textstyle}
           >
             About us
           </Link>
-          <Link style={{ color: "#da0404", textDecoration: "none" }} 
-          to="/Contact"
-          className={styles.textstyle}
+          <Link
+            style={{ color: "#da0404", textDecoration: "none" }}
+            to="/Contact"
+            className={styles.textstyle}
           >
-      
             Contact
           </Link>
         </>
-        
       );
     }
   }
 
-
-
   return (
     <>
-    <header className={`${styles.height} ${styles.grid}`} style={{position: 'relative'}}>
-      <div className={styles.toprow}>
-        <h3 style={{ color: "white", textAlign: "center" }}
-        className={styles.textstyle}
-        >
-          Announcements can go up here
-        </h3>
-      </div>
-      <div className={`${styles.bottomrow} ${styles.grid2}`}>
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <img
-            src="/headerlogo.png"
-            alt="thrift barn logo"
-            className={styles.logosize}
-          />
+      <header
+        className={`${styles.height} ${styles.grid}`}
+        style={{ position: "relative" }}
+      >
+        <div className={styles.toprow}>
+          <h3
+            style={{ color: "white", textAlign: "center" }}
+            className={styles.textstyle}
+          >
+            Announcements can go up here
+          </h3>
         </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            alignItems: "center",
-          }}
-        >
-          {showOptions()}
+        <div className={`${styles.bottomrow} ${styles.grid2}`}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src="/headerlogo.png"
+              alt="thrift barn logo"
+              className={styles.logosize}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+          >
+            {showOptions()}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <form className={styles.nosubmit}>
+              <input
+                type="search"
+                aria-label="Search"
+                className={styles.nosubmit}
+              />
+            </form>
+          </div>
         </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <form className={styles.nosubmit}>
-            <input type="search" aria-label="Search" className={styles.nosubmit}/>
-          </form>
-        </div>
-      </div>
-      <div className={`${styles.ddmenu}`} style={{ display: isVisible ? 'block' : 'none'}}><p>hello</p></div>
-    </header>
-    
+      </header>
     </>
   );
 }
