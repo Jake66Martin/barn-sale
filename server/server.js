0474147ce63fe175f6ed145fd4ba69520c8a3af2
leviathan <jake66martin@hotmail.com>
2025-01-22 17:35:39 -0500

    fixed

--- server/server.js
+++ server/server.js
@@ -42,13 +42,13 @@ const startApolloServer = async () => {
         });
     }
 
-    db.sync({force: false}).then(() => {
-        app.listen(PORT,   () => {
-            // app.listen(PORT, '0.0.0.0',  () => {
-            console.log(`API servers running on port ${PORT}`);
-            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`)
-        });
-    });
+    // db.sync({force: false}).then(() => {
+    //     app.listen(PORT,   () => {
+    //         // app.listen(PORT, '0.0.0.0',  () => {
+    //         console.log(`API servers running on port ${PORT}`);
+    //         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`)
+    //     });
+    // });
 
     if (process.env.NODE_ENV !== 'production') {
         db.sync({force: false}).then(() => {
