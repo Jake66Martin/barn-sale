2217338992e76f27706728dc763feec5983a8aed
leviathan <jake66martin@hotmail.com>
2025-01-23 19:44:09 -0500

    fixed footer

--- client/src/components/footer/footer2.jsx
+++ client/src/components/footer/footer2.jsx
@@ -3,10 +3,16 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function Footer2() {
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 715);
+  // const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 715);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+
+
+
 
   useEffect(() => {
-    const handleResize = () => setIsSmallScreen(window.innerWidth < 715);
+    const handleResize = () => setWindowWidth(window.innerWidth);
+
 
     // Add event listener to track window resizing
     window.addEventListener("resize", handleResize);
@@ -19,26 +25,26 @@ export default function Footer2() {
     
     return (
       <>
-        <footer className={`${styles.smallfooter}`}>
+        <footer className={`${styles.smallfooter}`} style={{position: 'relative'}}>
           
             <div className={`${styles.linkdiv}`} style={{display: 'flex', justifyContent: 'center', position: 'relative', top: '0'}}><p style={{color: 'white', fontSize: '30px'}} className={`${styles.textstyle}`}>Links</p></div>
-            <div className={`${styles.link1}`}><Link to='/' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px'}} className={`${styles.textstyle2}`}>Home</Link></div>
-            <div className={`${styles.link2}`}><Link to='/Browse' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px'}} className={`${styles.textstyle2}`}>Browse</Link></div>
-            <div className={`${styles.link3}`}><Link to="/Contact" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px'}} className={`${styles.textstyle2}`}>Contact</Link></div>
-            <div className={`${styles.link4}`}><Link to='/About' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px'}} className={`${styles.textstyle2}`}>About Us</Link></div>
+            <div className={`${styles.link1}`} style={{position: 'relative'}}><Link to='/' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px',}} className={`${styles.textstyle2}`}>Home</Link></div>
+            <div className={`${styles.link2}`} style={{position: 'relative'}}><Link to='/Browse' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px'}} className={`${styles.textstyle2}`}>Browse</Link></div>
+            <div className={`${styles.link3}`} style={{position: 'relative'}}><Link to="/Contact" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px'}} className={`${styles.textstyle2}`}>Contact</Link></div>
+            <div className={`${styles.link4}`} style={{position: 'relative'}}><Link to='/About' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', top: '5px'}} className={`${styles.textstyle2}`}>About Us</Link></div>
             
-            <div className={`${styles.furnicat}`}><p style={{color: 'white', display: 'flex', justifyContent: 'center', fontSize: '30px', position: 'relative', top: '20px'}} className={`${styles.textstyle}`}>Furniture Categories</p></div>
+            <div className={`${styles.furnicat}`}><p style={{color: 'white', display: 'flex', justifyContent: 'center', fontSize: '30px', position: 'relative', top: '20px'}} className={`${styles.textstyle} ${styles.furniline}`}>Furniture Categories</p></div>
             
-            <div className={`${styles.cat1}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} to='/Subcategories/LivingRoom' style={{textDecoration: 'none', color: 'white', position: 'relative', left: '25px'}} className={`${styles.textstyle2}`}>Living Room</Link></div>
-            <div className={`${styles.cat2}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', left: '47px'}} className={`${styles.textstyle2}`} to='/Subcategories/DiningRoom'>Dining Room</Link></div>
-            <div className={`${styles.cat3}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', left: '25px'}} className={`${styles.textstyle2}`} to='/Subcategories/Kitchen&Bath'>Kitchen & Bath</Link></div>
-            <div className={`${styles.cat4}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', left: '50px'}} className={`${styles.textstyle2}`} to='/Subcategories/HomeDecor'>Home Decor</Link></div>
-            <div className={`${styles.cat5}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', left: '25px'}} className={`${styles.textstyle2}`} to='/Subcategories/Child&Nursery'>Child & Nursery</Link></div>
-            <div className={`${styles.cat6}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', left: '75px'}} className={`${styles.textstyle2}`} to='/Subcategories/Bedroom'>Bedroom</Link></div>
-            <div className={`${styles.cat7}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', left: '25px'}} className={`${styles.textstyle2}`} to='/Subcategories/Garage&Exterior'>Garage & Exterior</Link></div>
-            <div className={`${styles.cat8}`}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white', position: 'relative', left: '100px'}} className={`${styles.textstyle2}`}>Office</Link></div>
+            <div className={`${styles.cat1}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} to='/Subcategories/LivingRoom' style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.posish}`}>Living Room</Link></div>
+            <div className={`${styles.cat2}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.dining}`} to='/Subcategories/DiningRoom'>Dining Room</Link></div>
+            <div className={`${styles.cat3}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.posish}`} to='/Subcategories/Kitchen&Bath'>Kitchen & Bath</Link></div>
+            <div className={`${styles.cat4}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.home}`} to='/Subcategories/HomeDecor'>Home Decor</Link></div>
+            <div className={`${styles.cat5}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.posish}`} to='/Subcategories/Child&Nursery'>Child & Nursery</Link></div>
+            <div className={`${styles.cat6}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.bed}`} to='/Subcategories/Bedroom'>Bedroom</Link></div>
+            <div className={`${styles.cat7}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.posish}`} to='/Subcategories/Garage&Exterior'>Garage & Exterior</Link></div>
+            <div className={`${styles.cat8}`} style={{position: 'relative'}}><Link onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} style={{textDecoration: 'none', color: 'white'}} className={`${styles.textstyle2} ${styles.office}`}>Office</Link></div>
 
-            <div className={`${styles.follow}`} style={{position: 'relative'}}><p style={{color: 'white', fontSize: '30px', display: 'flex', justifyContent: 'center'}} className={`${styles.textstyle}`}>Follow Us</p></div>
+            <div className={`${styles.follow}`} style={{position: 'relative'}}><p style={{color: 'white', fontSize: '30px', display: 'flex', justifyContent: 'center'}} className={`${styles.textstyle} ${styles.followline}`}>Follow Us</p></div>
             <div className={`${styles.followus}`}><a style={{textDecoration: 'none', color: 'white', display: 'flex', justifyContent: 'center', width: '100%',  height: '100%' }} className={`${styles.textstyle2}`} href='https://www.facebook.com/groups/961935455087635'>Follow us on facebook</a></div>
 
 
@@ -73,9 +79,11 @@ export default function Footer2() {
 
   return (
     <>
-      {isSmallScreen ? (
+      {windowWidth < 715 ? (
         phoneScreen()
-      ) : (
+      )
+      
+      : (
         <footer className={styles.footer}>
           <div style={{ display: "grid" }}>
             <div style={{ alignSelf: "center", justifySelf: "center" }}>
